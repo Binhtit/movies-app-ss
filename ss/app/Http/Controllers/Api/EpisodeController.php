0eea3b2ed7e81ba384883e295d4231276e6b131a
Dung Tran <thuydungtran0920@gmail.com>
2021-07-07 22:06:44 +0700

    fix api Episodes

--- ss/app/Http/Controllers/Api/EpisodeController.php
+++ ss/app/Http/Controllers/Api/EpisodeController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Episode;
 use Illuminate\Http\Request;
 
-class MenuController extends Controller
+class EpisodeController extends Controller
 {
     /**
      * Display a listing of the resource.
